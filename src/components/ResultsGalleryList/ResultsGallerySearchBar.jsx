c209a39eb2a5ef61a172c55057e43fa5aaa69501
SJCMN <sam.clavette@gmail.com>
2022-02-04 16:06:58 -0600

    layout for search bar

--- src/components/ResultsGalleryList/ResultsGallerySearchBar.jsx
+++ src/components/ResultsGalleryList/ResultsGallerySearchBar.jsx
@@ -1,30 +1,33 @@
-import { Box, Grid } from '@mui/material';
-import LocationComboBox from '../LocationComboBox/LocationComboBox';
-import TripDatePicker from '../TripDatePicker/TripDatePicker';
-import SearchBarButton from './SearchBarButton';
-import VehicleTypeDropdown from './VehicleTypeDropdown';
+import { Box, Grid } from "@mui/material";
+import LocationComboBox from "../LocationComboBox/LocationComboBox";
+import TripDatePicker from "../TripDatePicker/TripDatePicker";
+import SearchBarButton from "./SearchBarButton";
+import VehicleTypeDropdown from "./VehicleTypeDropdown";
 
 function ResultsGallerySearchBar() {
   return (
     <Box
       sx={{
-        display: 'flex',
+        display: "flex",
         flexGrow: 1,
-        width: '90%',
+        width: "70%",
         margin: 3,
+        
       }}
     >
-      <Grid item xs={6}>
-        <LocationComboBox />
-      </Grid>
-      <Grid item xs={4}>
-        <TripDatePicker />
-      </Grid>
-      <Grid item xs={4}>
-        <VehicleTypeDropdown />
-      </Grid>
-      <Grid item xs={4}>
-        <SearchBarButton />
+      <Grid container spacing={0}>
+        <Grid item xs={4} md={5}>
+          <LocationComboBox />
+        </Grid>
+        <Grid item xs={3} md={3}>
+          <TripDatePicker />
+        </Grid>
+        <Grid item xs={3} md={2}>
+          <VehicleTypeDropdown />
+        </Grid>
+        <Grid item xs={2} md={2}>
+          <SearchBarButton />
+        </Grid>
       </Grid>
     </Box>
   );
